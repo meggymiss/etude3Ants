f1418237b751a5a96a2157484631923442e599f9
sharmathias <68130536+sharmathias@users.noreply.github.com>
2020-07-15 17:22:35 +1200

    Update Ants.java

--- Ants.java
+++ Ants.java
@@ -1,19 +1,39 @@
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 public class Ants {
     int x;
     int y;
     char currState;
     char[] directions;
     char[] newState;
+    int numberIterations; 
 
     public void readIn() {
         Scanner reader = new Scanner (System.in);
         System.out.println("Enter initial state");
         this.currState = reader.nextChar() ; // need something that deals with not allowing spaces
+        for (int i =0; i<5; i++) {
+            this.directions[i] = reader.nextChar(); // should set the directions command
+        }
+        for (int i =0; i<5; i++) {
+            this.directions[i] = reader.nextChar(); // should set the directions command
+        }
     }
     public void initAnt (char currState, char[] directions, char[] newState) {
         
     }
-    public void output() {
+    public char output() {
+        char[] totalarray;
+        System.out.println(" ");// currState (has been changed) + directions[] + newStates
+        Sytem.out.println("# " + x + " " + y);
+    }
+    public void run (int iterations) {
+        Map<int, int> x = new HashMap<int int>;
+        Map<int, char> y = new HashMap<int, char>;
+        for (int i=0; i<iterations; i++) {
+            
+           
+        }
         
 }
