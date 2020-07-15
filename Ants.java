d67c7a7f5a356f01792af2323b2c9dde01405b1b
sharmathias <68130536+sharmathias@users.noreply.github.com>
2020-07-15 17:52:27 +1200

    Update Ants.java

--- Ants.java
+++ Ants.java
@@ -29,11 +29,43 @@ public class Ants {
         Sytem.out.println("# " + x + " " + y);
     }
     public void run (int iterations) {
-        Map<int, int> x = new HashMap<int int>;
-        Map<int, char> y = new HashMap<int, char>;
+        Map<int, int> x = new HashMap<int int>; // maps/hash tables that store x coordinates 
+        Map<int, char> y = new HashMap<int, char>;//that point to y coordinates, that point to a char holding the state
         for (int i=0; i<iterations; i++) {
-            
-           
+            if (x.containsKey(x) && y.containsKey(y)) {
+                char c = y.get(y); // will this work if there are multiple values of y?
+                //do what the rule is for c
+                // change what direction is
+            }
+            else{
+                //do the default state
+                //add the new value into the dictionary
+                //  if (x.containsKey(x)){
+                x.put(x, y);
+                y.put(y, state); // something like this
+          
+            }
+           newX(direction);
+           newY(direction);// updating thes values with these mutator methods
+        }  
+    }
+    public void newX (char dir) {
+        if (dir=='W') {
+            this.x=this.x-1;
         }
+        else if (dir == 'E') {
+            this.x = this.x+1;
+        } //would it be better to be returing an int??
         
+    }
+    public void newY (char dir) {
+        if (dir=='S') {
+            this.y=this.y-1;
+        }
+        else if (dir == 'E') {
+            this.y = this.y+1;
+        } //would it be better to be returing an int??
+        
+    }
 }
+
